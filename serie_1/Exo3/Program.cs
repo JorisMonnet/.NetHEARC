8a14533bd08256f3556ae1106b6a67ece3a0140a
Monnet Joris <joris.monnet@he-arc.ch>
2020-10-20 13:35:36 +0200

    fix Exo3 problems cf mail

--- serie_1/Exo3/Program.cs
+++ serie_1/Exo3/Program.cs
@@ -6,13 +6,13 @@ using static System.Console;
 namespace Exo3 {
     class Program {
         static void Main()  {
-            string[] lines = System.IO.File.ReadAllLines(@"./Mesures.txt");
+            //file in Exo3/bin/Debug/netcoreapp3.1/
+            string[] lines = System.IO.File.ReadAllLines("./Mesures.txt");
             StringBuilder str = new StringBuilder("Mesures.txt = \n");
             for (int i = 0; i < lines.Length; i++) {
                 str.Append((i % 10 != 0?", ":"\n") + lines[i]);
             }
             WriteLine(str.ToString());
-            ReadKey();
         }
     }
 }
