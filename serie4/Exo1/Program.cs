5654466169532bafb28746773019be539bd5159e
Monnet Joris <joris.monnet@he-arc.ch>
2020-10-22 13:48:41 +0200

    add directory

---
+++ serie4/Exo1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exo1 {
+    public static class ExtensionString {
+        public static void UpLowUp(this string s){
+            for(int i =0;i<s.Length;i++){
+                if(char.IsLower(s[i])){
+                    Console.Write(s[i].ToString().ToUpper());
+                }
+                else{
+                    Console.Write(s[i].ToString().ToLower());
+                }
+            }
+        } 
+    }
+    class Program {
+        static void Main(string[] args) {
+            string phrase = "Hugo, Ada et Jean jouent avec les robots.";
+            Console.WriteLine(phrase);
+            phrase.UpLowUp();
+        }
+    }
+}
