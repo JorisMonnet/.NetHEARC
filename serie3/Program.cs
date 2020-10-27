46e0476ee5b9b45e187170f15d1f2962ab280423
Monnet Joris <joris.monnet@he-arc.ch>
2020-10-27 20:34:01 +0100

    begin work on serie3

---
+++ serie3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace serie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
